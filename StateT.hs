7f7eae22dde1d93dca371c50ce6a09ba66ca5eb2
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2017-03-17 20:10:54 +0300

    Implement State monad transformer

--- StateT.hs
+++ StateT.hs
@@ -4,16 +4,16 @@ newtype State s a = State
   { runState :: s -> (a, s) }
 
 instance Functor (State s) where
-  fmap f x = State $ \s ->
-    let (a, s) = runState x $ s in
-    (f a, s)
+  fmap f mx = State $ \s ->
+    let (a, s') = runState mx $ s in
+    (f a, s')
 
 instance Applicative (State s) where
   pure x = State $ (,) x
-  f <*> mx = State $ \s ->
+  mf <*> mx = State $ \s ->
     let
       (a, s') = runState mx $ s
-      (func, s'') = runState f $ s'
+      (func, s'') = runState mf $ s'
     in
       (func a, s'')
 
@@ -21,3 +21,23 @@ instance Monad (State s) where
   mx >>= f = State $ \s ->
     let (a, s') = runState mx $ s in
     (runState $ f a) $ s'
+
+newtype StateIO s a = StateIO
+  { runStateIO :: s -> IO (a, s) }
+
+instance Functor (StateIO s) where
+  fmap f mx = StateIO $ \s -> do
+    (a, s') <- runStateIO mx $ s
+    return $ (f a, s')
+
+instance Applicative (StateIO s) where
+  pure x = StateIO $ \s -> return $ (x, s)
+  mf <*> mx = StateIO $ \s -> do
+    (a, s') <- runStateIO mx $ s
+    (f', s'') <- runStateIO mf $ s'
+    return (f' a, s'')
+
+instance Monad (StateIO s) where
+  mx >>= f = StateIO $ \s -> do
+    (a, s') <- runStateIO mx $ s
+    (runStateIO $ f a) $ s'
